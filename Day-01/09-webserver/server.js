9dd47fb641c13f66a1849cccf97413126f374a69
tkmagesh <tkmagesh77@gmail.com>
2015-08-16 16:20:12 +0530

    App Server - Seed

--- Day-01/09-webserver/server.js
+++ Day-01/09-webserver/server.js
@@ -1,13 +1,35 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 
-//path.join(__dirname, req.url)
-//res.statusCode = 404;
-//res.end();
+var staticResourceExtns = [".html",".jpg",".png",".ico",".js",".css"];
+function isStatic(resourceName){
+    return staticResourceExtns.indexOf(path.extname(resourceName)) !== -1;
+}
+
 var server = http.createServer(function(req, res){
-    console.log('a new connection is established for ', req.url, fs.existsSync(req.url));
-    res.write('<h1>Welcome to node.js</h1>');
-    res.end();
+    var resourceName = req.url;
+    if (isStatic(resourceName)){
+        var resourcePath = path.join(__dirname, req.url);
+        if (fs.existsSync(resourcePath)){
+            fs.createReadStream(resourcePath).pipe(res);
+        } else {
+            res.statusCode = 404;
+            res.end();
+        }
+    } else {
+        /*
+        1. parse the url using url.parse with query parsing option(do a require('url'))
+        2. if urlObj.pathname === '/calculator'
+            perform calculation & return result
+           else
+            res.statusCode = 404;
+
+        */
+    }
 });
 server.listen(8080);
 console.log('Server listening on port 8080 for resources at ', __dirname);
+
+/calculator?operation=add&n1=100&n2=200
+
